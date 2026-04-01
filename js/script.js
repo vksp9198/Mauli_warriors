77225f37031ac2cef3aa0f9853a40907fffcab17
VikasMain_git91 <vp9198776636@gmail.com>
2026-04-01 13:43:44 +0530

    links applied

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//sidebar starts
+// const sidebar = document.querySelector('.sidebar');
+
+// const showSidebar = document.querySelector('#showSidebar');
+// const hideSidebar = document.querySelector('#hideSidebar');
+
+// showSidebar.addEventListener('click', function (e) {
+//     sidebar.style.display = "flex";
+// })
+
+// hideSidebar.addEventListener('click', function (e) {
+//     sidebar.style.display = "none";
+// })
+
+
+
+
+
+const udev = document.querySelectorAll(".ud");
+console.log(udev, "ud clicked")
+udev.forEach((box) => {
+    box.addEventListener("click", function () {
+        alert("This service is currently under development and will be available soon. Thank you for your patience.");
+    })
+
+});
+
+
+
+
